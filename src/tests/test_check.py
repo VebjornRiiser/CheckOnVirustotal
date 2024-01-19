8dec884b77fb08ff41c9faf835547938e1e5655e
Vebjørn Riiser <12122688+VebjornRiiser@users.noreply.github.com>
2024-01-19 12:01:37 +0100

    Fixed filepath

--- src/tests/test_check.py
+++ src/tests/test_check.py
@@ -1,7 +1,7 @@
 from src import check
 
 filehash = "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855"
-filepath = "src/tests/file with spaces.txt"
+filepath = "./file with spaces.txt"
 
 
 def test_correct_hash():
