b362777ad4c8aa5735af298d0f0caad0e6901efd
Vebjørn Riiser <12122688+VebjornRiiser@users.noreply.github.com>
2024-01-23 09:14:59 +0100

    Fixed finding python path

--- src/install.py
+++ src/install.py
@@ -1,8 +1,18 @@
 import os
+import sys
 import winreg
 
-python_executable = os.path.join(os.__file__.split("lib\\")[0], "pythonw.exe")
+python_executable = os.path.join(os.path.dirname(sys.executable), "pythonw.exe")
 path_to_pyw_file = os.path.abspath(os.path.dirname(__file__)) + r"\check.pyw"
+
+if not os.path.exists(path_to_pyw_file):
+    print(f"File {path_to_pyw_file} not found")
+    input("Press Enter to exit...")
+    exit(1)
+if not os.path.exists(python_executable):
+    print(f"Pythonw executable not found")
+    input("Press Enter to exit...")
+    exit(1)
 
 winreg.SetValue(winreg.HKEY_CURRENT_USER,
                 r"SOFTWARE\Classes\*\shell\\virustotal_checker", 1,
